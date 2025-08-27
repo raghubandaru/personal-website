6bc52ad754e352ab5a9e38c51f247fa5344ba40e
Raghu Bandaru <raghubandaru45@gmail.com>
2025-08-27 02:24:30 +0000

    Add resume

---
+++ src/pages/resume.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ResumePage = () => {
+  return (
+    <Layout>
+      <a
+        href={
+          "https://drive.google.com/file/d/1ss5Hs7VWg791mdJfzGEGDrJtVuYXCHIQ/view?usp=drive_link"
+        }
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Raghu Bandaru's Resume
+      </a>
+    </Layout>
+  )
+}
+
+export default ResumePage
